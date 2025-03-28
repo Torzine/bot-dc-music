81cccaa55f6f51b019103c46ac9642d7ae20c280
Torzine <fannyeki96@gmail.com>
2025-03-28 12:26:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ intents.message_content = True
 intents.voice_states = True  # Diperlukan untuk voice
 
 # Inisialisasi bot
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="?", intents=intents)
 
 @bot.event
 async def on_ready():
